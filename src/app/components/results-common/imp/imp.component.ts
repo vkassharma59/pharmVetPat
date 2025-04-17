b00849456165e7d45330e7b55eb62ad25b99b0fb
vikas sharma <vikas.sharma59@gamil.com>
2025-04-17 15:32:51 +0530

    filter bug fixes

--- src/app/components/results-common/imp/imp.component.ts
+++ src/app/components/results-common/imp/imp.component.ts
@@ -1,17 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { UtilityService } from '../../../services/utility-service/utility.service';
-import { Auth_operations } from '../../../Utils/SetToken';
-import { ImageModalComponent } from '../../../commons/image-modal/image-modal.component';
 import { CommonModule } from '@angular/common';
-import { environment } from '../../../../environments/environment';
 import { ImpPatentsCardComponent } from '../imp-patents-card/imp-patents-card.component';
 import { ChildPagingComponent } from '../../../commons/child-paging/child-paging.component';
 import { MainSearchService } from '../../../services/main-search/main-search.service';
+import { TruncatePipe } from '../../../pipes/truncate.pipe';
+
 @Component({
   selector: 'chem-imp',
   standalone: true,
-  imports: [CommonModule,ImpPatentsCardComponent, ChildPagingComponent],
+  imports: [TruncatePipe, CommonModule,ImpPatentsCardComponent, ChildPagingComponent],
   templateUrl: './imp.component.html',
   styleUrl: './imp.component.css'
 })
