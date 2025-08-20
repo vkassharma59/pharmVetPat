afcfc4f18d1419bd0c078f568082a347678a1132
suraj sahu <sahu.suraj@hotmail.com>
2025-08-20 18:35:07 +0530

    header part changes,add the japan filter ui

--- src/app/shared-ros.service.ts
+++ src/app/shared-ros.service.ts
@@ -5,9 +5,8 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class SharedRosService {
-
+ private storageKey = 'lastSearchData';
   private rosCountSubject = new BehaviorSubject<{ agrochemical: number, pharmaceutical: number, index: any } | null>(null);
-
   // Observable to subscribe to
   rosCount$ = this.rosCountSubject.asObservable();
 
@@ -25,5 +24,47 @@ export class SharedRosService {
 
   getAllDataSets(): any[] {
     return this.allDataSetsSubject.getValue();
+  }
+   
+
+//  setSearchData(searchType: string, keyword: string, criteria: any): void {
+//     const data = { searchType, keyword, criteria };
+//     localStorage.setItem('lastSearchData', JSON.stringify(data));
+//   }
+
+   setSearchData(searchType: string, keyword: string, criteria: any): void {
+    this.clearSearchData();
+
+    let displayValue = '';
+
+    // CASE 1: Advanced Search (criteria is array)
+    if (Array.isArray(criteria) && criteria.length > 0) {
+      displayValue = criteria.map(c => `${c.column}: ${c.keyword}`).join(', ');
+    }
+    // CASE 2b: Column-based Simple Search (criteria is string + keyword)
+    else if (typeof criteria === 'string' && criteria.trim() !== '' && keyword) {
+      displayValue = `${criteria}: ${keyword}`;
+    }
+    // CASE 2a: Normal Simple Search (only keyword)
+    else if (keyword) {
+      displayValue = keyword;
+    }
+    // Fallback
+    else {
+      displayValue = 'No search data';
+    }
+
+    const data = { searchType, keyword, criteria, displayValue };
+    localStorage.setItem(this.storageKey, JSON.stringify(data));
+  }
+  // Get last search data
+  getSearchData(): { searchType: string, keyword: string, criteria: any } | null {
+    const data = localStorage.getItem('lastSearchData');
+    return data ? JSON.parse(data) : null;
+  }
+
+  // Clear
+  clearSearchData(): void {
+    localStorage.removeItem('lastSearchData');
   }
 }
