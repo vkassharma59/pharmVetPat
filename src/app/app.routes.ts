f32ea88eb5b4783622b76e2d633555e94eaea92f
Himanshi Vyas <himanshivyas2005@gmail.com>
2025-09-01 18:28:36 +0530

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,8 @@
 import { Routes } from '@angular/router';
-export const routes: Routes = []
+import { pharmaDatabaseSearchComponent } from './components/pharma-database-search/pharma-database-search.component';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export const routes: Routes = [
+  { path: 'pharma-database-search', component: pharmaDatabaseSearchComponent },
+  { path: '', redirectTo: 'pharma-database-search', pathMatch: 'full' } // optional default route
+];
 
